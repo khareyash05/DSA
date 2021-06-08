b72defe5c74e0de1e1cbedc30e695367872a5475
khareyash05 <>
2021-06-08 16:28:45 +0000

    triplet less than give

---
+++ tripletless.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int counttriplet(int arr[],int x){
+  int ans=2;
+  return ans;
+}
+int main(){
+  int arr[] = {3,5,2};
+  int x;
+  cin >> x;
+  int ans = counttriplet(arr, x);
+  cout << ans;
+}
