094252e28e577a88d7f41856e7c4f07b5d3a89d6
Yash Khare <>
2021-06-08 17:10:57 +0000

    hashing

---
+++ hashing/union.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int union1(int a[],int b[]){
+  unordered_set<int>set;
+  for(int x:a[])
+    set.insert(x);
+  for(int x:b)  
+    set.insert(x);
+  return set.size();
+}
+int main(){
+  int a[]={3,4,5,6};
+  int b[]= {2,3,5,7};
+  cout << union1(a,b);
+}
