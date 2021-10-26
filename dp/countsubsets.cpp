808a474e2c9150d9b8b98b2f1878ac1736e40480
Yash Khare <60147732+khareyash05@users.noreply.github.com>
2021-10-26 06:51:07 +0530

    count subsets with given sum

--- dp/countsubsets.cpp
+++ dp/countsubsets.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 bool dp[101][101]={false};
-bool subsum(int arr[],int sum,int n){
+int countsum(int arr[],int sum,int n){
     // Initialisation COndition
     for (int i = 0; i <= n; i++)
         dp[i][0] = true; // Remember when sum is 0-> with any no. of elements there can be any empty subset answer hence true in initialisation
@@ -16,10 +16,15 @@ bool subsum(int arr[],int sum,int n){
                 dp[i][j] = dp[i-1][j];
         }
     }
-    return dp[n][sum];
+    int count=0;
+    for(int i=1;i<n+1;i++){
+        if(dp[i][10]==true)
+            count++;
+    }
+    return count;
 }
 int main(){
     int arr[] = {3, 34, 4, 12, 5, 2 };
     int sum=7;
-    cout << subsum(arr,sum,5);
+    cout << countsum(arr,sum,6);
 }
