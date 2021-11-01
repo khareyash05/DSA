18946a727a10911717062d8f71295cf1047482b7
Yash Khare <60147732+khareyash05@users.noreply.github.com>
2021-11-01 23:09:22 +0530

    longest common subsequence

---
+++ dp/lcs.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Recursive Approach
+int lcs(string X,string Y,int n,int m){
+    if(n==0||m==0) return 0;
+    else{
+        if(X[n-1]==Y[m-1]) return 1+lcs(X,Y,n-1,m-1);
+        else 
+            return max(lcs(X,Y,n-1,m),lcs(X,Y,n,m-1));
+    }
+}
+// Top-Down Approach
+int lcs1(string X,string Y,int n,int m){
+    int dp[n+1][m+1];
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<m+1;j++){
+            if(i==0||j==0) dp[i][j]=0;
+        }
+    }
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            if(X[i]==Y[j])
+                dp[i][j]=1+dp[i-1][j-1];
+            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);    
+        }
+    }
+    return dp[n][m];
+}
+int main(){
+    string X = "abcdef";
+    string Y = "abdefg";
+    int n = 6;
+    int m=6;
+    cout << lcs1(X,Y,n,m);
+}
