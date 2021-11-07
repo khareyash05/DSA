7bc3914ee14bfbac940d72483cd93b94e3f6ffcc
Yash Khare <60147732+khareyash05@users.noreply.github.com>
2021-11-07 22:49:20 +0530

    Longest Palindromic Subsequence

---
+++ dp/lognestpalindromesubse.cpp
@@ -0,0 +1,28 @@
+// Longest Palindromic Subsequence is lcs(a,reverse(a))
+#include<bits/stdc++.h>
+using namespace std;
+int lcs(string X,string Y,int n,int m){
+    int dp[n+1][m+1];
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<m+1;j++){
+            if(i==0||j==0) dp[i][j]=0;
+        }
+    }
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            if(X[i-1]==Y[j-1])
+                dp[i][j]=1+dp[i-1][j-1];
+            else 
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);    
+        }
+    }
+    return dp[n][m];
+}
+int lps(string X,int n){
+    string Y = string(X.rbegin(),X.rend());
+    return lcs(X,Y,n,n);
+}
+int main(){
+    string X = "agbcba";
+    cout << lps(X,6);
+}
